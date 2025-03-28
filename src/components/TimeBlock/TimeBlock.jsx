2f97794e4b403f67d172e6552713cfba56e3a054
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-03-28 22:53:35 +0100

    refactor to hook

--- src/components/TimeBlock/TimeBlock.jsx
+++ src/components/TimeBlock/TimeBlock.jsx
@@ -1,9 +1,11 @@
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./TimeBlock.module.css";
 
 import { calculateDurationInHours } from "../../utils/calculateDurationUtils";
 import { useState } from "react";
 import { calculateOneHourRepresentationInPixels } from "../../utils/calculateTimeRepresentationUtils";
+
+import { useResizeTimeBlock } from "../../hooks/useResizeTimeBlock";
 
 // Räknar ut hur många enheter brett tidsblocket ska vara baserat på timmar i deciamlform.
 const initialDuration = calculateDurationInHours(
@@ -13,137 +15,47 @@ const initialDuration = calculateDurationInHours(
 
 console.log("initial duration: ", initialDuration);
 
-// const timelineSize = 3;
-
 export default function TimeBlock({ timelineSize }) {
-  console.log("fick in timeline sisa", timelineSize);
-  //-----------------------------------------------------
-  // VARIABLAR   // VARIABLAR  // VARIABLAR  // VARIABLAR
-  const tidsblockRef = useRef();
-
-  const [isResizing, setIsResizing] = useState(false);
-
-  const [duration, setDuration] = useState(initialDuration);
-
-  const [initialMousePositionRight, setInitialMousePositionRight] =
-    useState(null);
 
   // Beskriver hur mycket en timme motsvarar i pixlar på skärmen:
-  const [pixelRepresentationOfOneHour, setPixelRepresentationOfOneHour] =
-    useState(16);
-
+  const [pixelRepresentationOfOneHour, setPixelRepresentationOfOneHour] = useState(16);
   //beskriver med deciamler hur mycket EN pixel på skärmen är värd i tid:
-  const [hourRepresentationOfOnePixel, setHourRepresentationOfOnePixel] =
-    useState(1 / 16);
+  const [hourRepresentationOfOnePixel, setHourRepresentationOfOnePixel] = useState(1 / 16);
 
-  //-----------------------------------------------------
-  // FUNKTIONER  // FUNKTIONER  // FUNKTIONER  // FUNKTIONER
-
-  function startResizing(e) {
-    setIsResizing(true);
-    setInitialMousePositionRight(e.clientX);
-    // console.log(e.clientX);
-    // console.log("resizing startar");
-  }
+  const [isResizing, setIsResizing] = useState(false);
+  const [duration, setDuration] = useState(initialDuration);
+  const [initialMousePositionRight, setInitialMousePositionRight] = useState(null);
 
   function stopResizing() {
     setIsResizing(false);
     // console.log("resizing stopad");
   }
 
+  function startResizingRight(e) {
+    setIsResizing(true);
+    setInitialMousePositionRight(e.clientX);
+    // console.log(e.clientX);
+    // console.log("resizing startar");
+  }
+
   useEffect(() => {
     //sätter hur många pixlar 1h ska motsvara baserat på remstorlek:
-    setPixelRepresentationOfOneHour(
-      calculateOneHourRepresentationInPixels(timelineSize)
-    );
+    setPixelRepresentationOfOneHour(calculateOneHourRepresentationInPixels(timelineSize));
     //...Räknar ut hur mycket en pixel är värd i tid.
-    setHourRepresentationOfOnePixel(
-      1 / calculateOneHourRepresentationInPixels(timelineSize)
-    );
+    setHourRepresentationOfOnePixel(1 / calculateOneHourRepresentationInPixels(timelineSize));
   }, [timelineSize]);
 
-  // const resizing = useCallback(
-  //   (e) => {
-  //     if (isResizing) {
-  //       console.log("vi movar");
-  //       //varje gång musen flyttar sig en pixel tar vi reda på diffen
-  //       const oldPosition = initialMousePositionRight;
-  //       const newPosition = e.clientX;
 
-  //       const sizeDiff = newPosition - oldPosition;
 
-  //       const durationDiff = hourRepresentationOfOnePixel * sizeDiff;
-
-  //       console.log(sizeDiff);
-  //       console.log(durationDiff);
-
-  //       setInitialMousePositionRight(newPosition);
-  //       setDuration(duration + durationDiff);
-
-  //       // setDuration(duration + 0.1);
-  //       console.log(e.clientX);
-  //     }
-  //   },
-  //   [
-  //     isResizing,
-  //     duration,
-  //     hourRepresentationOfOnePixel,
-  //     initialMousePositionRight,
-  //   ]
-  // );
-
-  // kollar Resizing pågor och gör logik isåfall.
-  // Triggas när isResizing slårs på eller av
-  useEffect(() => {
-    function resizing(e) {
-      if (isResizing) {
-        console.log("vi movar");
-        //varje gång musen flyttar sig en pixel tar vi reda på diffen
-        const oldPosition = initialMousePositionRight;
-        const newPosition = e.clientX;
-
-        const sizeDiff = newPosition - oldPosition;
-
-        const durationDiff = hourRepresentationOfOnePixel * sizeDiff;
-
-        console.log(sizeDiff);
-        console.log(durationDiff);
-
-        setInitialMousePositionRight(newPosition);
-        setDuration(duration + durationDiff);
-
-        // setDuration(duration + 0.1);
-        console.log(e.clientX);
-      }
-    }
-
-    if (isResizing) {
-      //disablear möjligheten att råka markera text:
-      document.body.style.userSelect = "none";
-      document.body.style.webkitUserSelect = "none";
-
-      // Lägg till eventlyssnare på musen
-      document.addEventListener("mousemove", resizing);
-      document.addEventListener("mouseup", stopResizing);
-    } else {
-      //Man får nu markera text igen när resizing är avstängd
-      document.body.style.userSelect = "auto";
-      document.body.style.webkitUserSelect = "auto";
-    }
-
-    return () => {
-      // Rensa upp event listeners när komponenten avmonteras eller när resizing slutar
-      document.removeEventListener("mousemove", resizing);
-      document.removeEventListener("mouseup", stopResizing);
-      document.body.style.userSelect = "auto";
-      document.body.style.webkitUserSelect = "auto";
-    };
-  }, [
-    isResizing,
-    duration,
-    hourRepresentationOfOnePixel,
-    initialMousePositionRight,
-  ]);
+  useResizeTimeBlock({
+    isResizing: isResizing,
+    stopResizing: stopResizing,
+    duration: duration,
+    setDuration: setDuration,
+    hourRepresentationOfOnePixel: hourRepresentationOfOnePixel,
+    initialMousePositionRight: initialMousePositionRight,
+    setInitialMousePositionRight: setInitialMousePositionRight,
+  });
 
   return (
     <>
@@ -155,13 +67,12 @@ export default function TimeBlock({ timelineSize }) {
       <br />
       Initial musposition höger: {initialMousePositionRight} */}
       <div
-        ref={tidsblockRef}
         style={{ width: `${duration * pixelRepresentationOfOneHour}px` }}
         className={styles.resizeable}
       >
         <div
           className={styles.handle}
-          onMouseDown={startResizing}
+          onMouseDown={startResizingRight}
           onMouseUp={stopResizing}
         ></div>
       </div>
--- src/hooks/useResizeTimeBlock.js
+++ src/hooks/useResizeTimeBlock.js
@@ -1,11 +1,67 @@
 import { useEffect } from "react";
+import PropTypes from "prop-types";
 
-export function useResizeTimeBlock() {
+export function useResizeTimeBlock({
+  isResizing,
+  stopResizing,
+  duration,
+  setDuration,
+  hourRepresentationOfOnePixel,
+  initialMousePositionRight,
+  setInitialMousePositionRight,
+}) {
   useEffect(() => {
-    //logik
+
+    if (isResizing) {
+      //disablear möjligheten att råka markera text:
+      document.body.style.userSelect = "none";
+      document.body.style.webkitUserSelect = "none";
+
+      // Lägg till eventlyssnare på musen
+      document.addEventListener("mousemove", resizing);
+      document.addEventListener("mouseup", stopResizing);
+    } else {
+      //Man får markera text när resizing är avstängd
+      document.body.style.userSelect = "auto";
+      document.body.style.webkitUserSelect = "auto";
+    }
+
+    function resizing(e) {
+      if (isResizing) {
+        console.log("vi movar");
+
+        //varje gång musen flyttar sig en pixel tar vi reda på diffen
+        const oldPosition = initialMousePositionRight;
+        const newPosition = e.clientX;
+
+        const positionDiff = newPosition - oldPosition;
+        const durationDiff = hourRepresentationOfOnePixel * positionDiff;
+
+        console.log(
+          "Diff muspek. Position:", positionDiff, "px |", "Diff duration: ", durationDiff, "h"
+        );
+
+        setInitialMousePositionRight(newPosition);
+        setDuration(duration + durationDiff);
+      }
+    }
+
+
 
     return () => {
-      //beskrivande kommentar
+      // Rensa upp event listeners när komponenten avmonteras eller när resizing slutar
+      document.removeEventListener("mousemove", resizing);
+      document.removeEventListener("mouseup", stopResizing);
+      document.body.style.userSelect = "auto";
+      document.body.style.webkitUserSelect = "auto";
     };
-  }, []);
+  }, [
+    duration,
+    hourRepresentationOfOnePixel,
+    initialMousePositionRight,
+    isResizing,
+    setDuration,
+    setInitialMousePositionRight,
+    stopResizing,
+  ]);
 }
